43b6ede852ac46fdd7f2bfcb66222980b5d4d64c
Ricky Rhodes <ricky@rickyrhodes.com>
2020-08-06 18:27:44 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('document').on('keydown', function() {
+  $('document').on('keydown', function(key) {
     if (key.which == 71) {
       alert('You have pressed G.')
     }
